b88edd120b042163a4dfe7692e1a558d1feb4f03
ibaiq <416322112@qq.com>
2021-12-18 14:05:48 +0800

    换一种导入方式，编辑器有提示

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore } from "vuex";
 import getters from "./getters.js";
-// import { user, menu } from "./community/index.js";
 import modules from "./community/index.js";
 
 export default createStore({
